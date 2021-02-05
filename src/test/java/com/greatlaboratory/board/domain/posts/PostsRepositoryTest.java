3e990f60b6368033d1f41748520c9750149aa637
Myung-gwan Kim <cartopia95@naver.com>
2021-02-05 15:03:11 +0900

    test - read기능과 jpa auditing 기능 테스트

---
+++ src/test/java/com/greatlaboratory/board/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,64 @@
+package com.greatlaboratory.board.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest // 별다른 설정 없이 이 어노테이션을 사용하면 자동으로 h2데이터베이스를 실행한다.
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void findPosts(){
+        // given
+        String title = "test title";
+        String content = "test content";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("greatlaboratory")
+                .build());
+
+        // when
+        List<Posts> postsList =  postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntityAudit(){
+        // given
+        LocalDateTime now = LocalDateTime.of(2021,2,3,0,0,0);
+        LocalDateTime tomorrow = LocalDateTime.of(2021,2,4,0,0,0);
+        postsRepository.save(Posts.builder().title("title").content("content").author("author").build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        System.out.println(">>>>>>>>>>>>> createdDate="+posts.getCreatedDate()+", modifiedDate="+posts.getModifiedDate());
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isBefore(tomorrow);
+    }
+}
