ee79db1974b4ede0013b7cdc443d764afa822bb6
Myung-gwan Kim <cartopia95@naver.com>
2021-02-17 20:06:28 +0900

    fix - 가짜로 인증된 사용자 권한 부여

--- src/test/java/com/greatlaboratory/board/web/HelloControllerTest.java
+++ src/test/java/com/greatlaboratory/board/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.greatlaboratory.board.web;
 
+import com.greatlaboratory.board.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,13 +16,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+            excludeFilters = {
+                // @WebMvcTest는 SecurityConfig를 스캔할 수 있지만 SecurityConfig 내부의 CustomOAuth2UserService는 스캔 못한다.
+                // 아예 스캔 대상에서 SecurityConfig을 필터로 걸러야 에러가 나지 않는다.
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+            })
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
     @Test
+    @WithMockUser(roles = "USER") // USER권한으로 가짜 인증된 사용자를 Mock으로 만든다.
     public void helloTest() throws Exception{
         String hello = "Hello";
 
@@ -28,6 +38,7 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void helloDtoTest() throws Exception{
         String name = "hello";
         int amount = 1000;
