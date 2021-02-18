45e99772dbdb133e3f531bc7deae1f337b87e569
Myung-gwan Kim <cartopia95@naver.com>
2021-02-18 20:44:46 +0900

    fix - 가짜 인증된 사용자 추가를 위해 TestRestTemplate제거 후 MockMvc 추가

--- src/test/java/com/greatlaboratory/board/web/PostsApiControllerTest.java
+++ src/test/java/com/greatlaboratory/board/web/PostsApiControllerTest.java
@@ -1,25 +1,32 @@
 package com.greatlaboratory.board.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.greatlaboratory.board.domain.posts.Posts;
 import com.greatlaboratory.board.domain.posts.PostsRepository;
 import com.greatlaboratory.board.web.dto.PostsSaveRequestDto;
 import com.greatlaboratory.board.web.dto.PostsUpdateRequestDto;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -29,10 +36,17 @@ public class PostsApiControllerTest {
     private int port;
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private PostsRepository postsRepository;
 
     @Autowired
-    private PostsRepository postsRepository;
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setup(){
+        mvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+    }
 
     @After
     public void tearDown() throws Exception {
@@ -40,6 +54,7 @@ public class PostsApiControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER") // 해당 어노테이션은 MockMvc에서만 작동, USER권한을 가지는 가짜 사용자 만들어서 사용
     public void PostsSave() throws Exception {
         // given
         String title = "test title";
@@ -52,23 +67,29 @@ public class PostsApiControllerTest {
         String url = "http://localhost:"+port+"/api/v1/posts";
 
         // when
-        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+        mvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk())
+                .andExpect(content().string("1"));
 
         // then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
-        assertThat(responseEntity.getBody()).isEqualTo(1L);
         List<Posts> postsList = postsRepository.findAll();
         assertThat(postsList.get(0).getTitle()).isEqualTo(title);
         assertThat(postsList.get(0).getContent()).isEqualTo(content);
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void PostsUpdate() throws Exception {
         // given
         String title = "test title";
         String content = "test content";
-        Posts savedPost = postsRepository.save(Posts.builder().title(title).content(content).author("greatlaboratory").build());
+        Posts savedPost = postsRepository.save(Posts.builder()
+                                                    .title(title)
+                                                    .content(content)
+                                                    .author("greatlaboratory")
+                                                    .build());
 
         Long updateId = savedPost.getId();
         String updatedTitle = "updated test title";
@@ -79,15 +100,16 @@ public class PostsApiControllerTest {
                 .content(updatedContent)
                 .build();
         String url = "http://localhost:"+port+"/api/v1/posts/"+updateId;
-        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
 
         // when
-        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+        mvc.perform(put(url)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk())
+                .andExpect(content().string("2")); // PostsSave 테스트 메소드에서 이미 하나를 만들었기에 여기서 만든건 아이디가 2가 된다.
+
 
         // then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
-        assertThat(responseEntity.getBody()).isEqualTo(2L); // PostsSave 테스트 메소드에서 이미 하나를 만들었기에 여기서 만든건 아이디가 2가 된다.
         List<Posts> postsList = postsRepository.findAll();
         assertThat(postsList.get(0).getTitle()).isEqualTo(updatedTitle);
         assertThat(postsList.get(0).getContent()).isEqualTo(updatedContent);
